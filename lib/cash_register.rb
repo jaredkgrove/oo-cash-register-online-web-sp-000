bcc44edea3fa4d66505354fc5dcc40bd5616ed3a
Jared Grove <jaredkgrove@gmail.com>
2019-03-28 02:45:02 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
   end
   
   def apply_discount(discount)
-    self.total = self.total * discount
+    self.total = self.total * (100 - discount) / 100
   end
   
   def void_last_transaction
